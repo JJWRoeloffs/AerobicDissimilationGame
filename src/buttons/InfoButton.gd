1ea5ac0259132d6508db496efd941f048f5e672a
aster <s2955199@vuw.leidenuniv.nl>
2022-11-28 15:00:07 +0100

    error fix

--- src/buttons/InfoButton.gd
+++ src/buttons/InfoButton.gd
@@ -3,22 +3,22 @@ extends Button
 
 #export(String, FILE) var next_scene_path: = ""
 
-onready var scene_tree: = get_tree()
-onready var info_overlay: ColorRect = get_node("InfoRect")
-
-var paused: = false setget set_paused
-
-func _unhandled_input(event: InputEvent) -> void:
-    if signal._on_InfoButton_up():
-        paused = not paused
-        #get_tree().change_scene(next_scene_path)
-        info_overlay.visible = true
-
-func set_paused(value: bool=true) -> void:
-    paused = value
-    scene_tree.paused = value
-    info_overlay.visible = value
-    
+#onready var scene_tree: = get_tree()
+#onready var info_overlay: ColorRect = get_node("InfoRect")
+#
+#var paused: = false setget set_paused
+#
+#func _unhandled_input(event: InputEvent) -> void:
+#    if signal._on_InfoButton_up():
+#        paused = not paused
+#        #get_tree().change_scene(next_scene_path)
+#        info_overlay.visible = true
+#
+#func set_paused(value: bool=true) -> void:
+#    paused = value
+#    scene_tree.paused = value
+#    info_overlay.visible = value
+#
 #func _get_configuration_warning() -> String:
 #    if next_scene_path == "":
 #        return "next_scene_path has to be set for button to work" 
