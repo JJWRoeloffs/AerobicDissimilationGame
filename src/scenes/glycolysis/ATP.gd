9432e8d14f84ae7f13231ed62a313bc788ca4cee
JJWRoeloffs <jelleroeloffs@gmail.com>
2022-12-04 14:15:14 +0100

    Fix ATP size

--- src/scenes/glycolysis/ATP.gd
+++ src/scenes/glycolysis/ATP.gd
@@ -3,8 +3,5 @@ extends RigidBody2D
 export var min_speed: = 150
 export var max_speed: = 250
 
-func _ready() -> void:
-    scale = Vector2(0.25, 0.25)
-
 func _on_screen_exited() -> void:
     queue_free()
