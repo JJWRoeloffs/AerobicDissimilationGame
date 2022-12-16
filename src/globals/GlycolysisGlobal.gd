ecaf85562cbd766e3e3ce464cdd8401e1ca8d6b0
JJWRoeloffs <jelleroeloffs@gmail.com>
2022-12-16 10:23:35 +0100

    Make the second stage take four pick-ups

--- src/globals/GlycolysisGlobal.gd
+++ src/globals/GlycolysisGlobal.gd
@@ -10,5 +10,7 @@ var score:= 0 setget set_score
 func set_score(value: int) -> void:
     score = value
     emit_signal("score_changed")
-    if score > 1:
+    if score > 1 and first_part == true:
+        emit_signal("change_scene")
+    elif score > 3:
         emit_signal("change_scene")
